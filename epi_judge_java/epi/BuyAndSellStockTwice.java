5da21c1ad1f89b41f8c52fddd3e24b462798af23
dkoval <koval.dmitriy@gmail.com>
2018-02-03 23:45:03 +0100

    add solution to BuyAndSellStockTwice problem

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -3,17 +3,34 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTestHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BuyAndSellStockTwice {
-  @EpiTest(testfile = "buy_and_sell_stock_twice.tsv")
-  public static double buyAndSellStockTwice(List<Double> prices) {
-    // Implement this placeholder.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    GenericTestHandler.executeTestsByAnnotation(
-        new Object() {}.getClass().getEnclosingClass(), args);
-  }
+    // O(N) time complexity, O(N) space complexity
+    @EpiTest(testfile = "buy_and_sell_stock_twice.tsv")
+    public static double buyAndSellStockTwice(List<Double> prices) {
+        double profit = 0.0;
+        // forward phase
+        double minPriceSoFar = Double.MAX_VALUE;
+        List<Double> firstBuyAndSellProfits = new ArrayList<>();
+        for (Double price : prices) {
+            minPriceSoFar = Math.min(minPriceSoFar, price);
+            profit = Math.max(profit, price - minPriceSoFar);
+            firstBuyAndSellProfits.add(profit);
+        }
+        // backward phase
+        double maxPriceSoFar = Double.MIN_VALUE;
+        for (int i = prices.size() - 1; i > 0; i--) {
+            maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
+            profit = Math.max(profit, maxPriceSoFar - prices.get(i) + firstBuyAndSellProfits.get(i - 1));
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        GenericTestHandler.executeTestsByAnnotation(
+                new Object() {}.getClass().getEnclosingClass(), args);
+    }
 }
