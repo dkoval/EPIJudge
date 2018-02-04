b7153de1ce7e165e03810ca05a7dc8b5ca147552
dkoval <koval.dmitriy@gmail.com>
2018-02-04 20:48:31 +0100

    add solution to BuyAndSellStock problem

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -6,14 +6,20 @@ import epi.test_framework.GenericTestHandler;
 import java.util.List;
 
 public class BuyAndSellStock {
-  @EpiTest(testfile = "buy_and_sell_stock.tsv")
-  public static double computeMaxProfit(List<Double> prices) {
-    // Implement this placeholder.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    GenericTestHandler.executeTestsByAnnotation(
-        new Object() {}.getClass().getEnclosingClass(), args);
-  }
+    @EpiTest(testfile = "buy_and_sell_stock.tsv")
+    public static double computeMaxProfit(List<Double> prices) {
+        double maxProfit = 0.0;
+        double minPriceSoFar = Double.MAX_VALUE;
+        for (Double price : prices) {
+            minPriceSoFar = Math.min(minPriceSoFar, price);
+            maxProfit = Math.max(maxProfit, price - minPriceSoFar);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        GenericTestHandler.executeTestsByAnnotation(
+                new Object() {}.getClass().getEnclosingClass(), args);
+    }
 }
