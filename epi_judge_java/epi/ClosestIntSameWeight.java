aa0d9d83cf51b5df78a1f687b8ca13f1a2c46e2d
dkoval <koval.dmitriy@gmail.com>
2018-03-17 13:57:49 +0100

    add solution to `ClosestIntSameWeight` problem

--- epi_judge_java/epi/ClosestIntSameWeight.java
+++ epi_judge_java/epi/ClosestIntSameWeight.java
@@ -4,14 +4,23 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTestHandler;
 
 public class ClosestIntSameWeight {
-  @EpiTest(testfile = "closest_int_same_weight.tsv")
-  public static long closestIntSameBitCount(long x) {
-    // Implement this placeholder.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    GenericTestHandler.executeTestsByAnnotation(
-        new Object() {}.getClass().getEnclosingClass(), args);
-  }
+    @EpiTest(testfile = "closest_int_same_weight.tsv")
+    public static long closestIntSameBitCount(long x) {
+        for (int i = 0; i < 63; i++) {
+            // find two consecutive bit that differ
+            if (((x >> i) & 1) != ((x >> (i + 1)) & 1)) {
+                // and then swap them
+                long mask = (1L << i) | (1L << (i + 1));
+                return x ^ mask;
+            }
+        }
+        throw new IllegalArgumentException("All bits are either 0 or 1. Input: " + x);
+    }
+
+    public static void main(String[] args) {
+        GenericTestHandler.executeTestsByAnnotation(
+                new Object() {
+                }.getClass().getEnclosingClass(), args);
+    }
 }
