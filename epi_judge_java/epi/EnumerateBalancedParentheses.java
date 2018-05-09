cdfe13511e77240eaa3a18401ffb7bd8c3b6e23e
dkoval <koval.dmitriy@gmail.com>
2018-05-09 15:01:01 +0200

    added solution to `EnumerateBalancedParentheses` problem

--- epi_judge_java/epi/EnumerateBalancedParentheses.java
+++ epi_judge_java/epi/EnumerateBalancedParentheses.java
@@ -5,30 +5,50 @@ import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTestHandler;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiPredicate;
 
 public class EnumerateBalancedParentheses {
-  @EpiTest(testfile = "enumerate_balanced_parentheses.tsv")
 
-  public static List<String> generateBalancedParentheses(int numPairs) {
-    // Implement this placeholder.
-    return null;
-  }
-
-  @EpiTestComparator
-  public static BiPredicate<List<String>, List<String>> comp =
-      (expected, result) -> {
-    if (result == null) {
-      return false;
+    @EpiTest(testfile = "enumerate_balanced_parentheses.tsv")
+    public static List<String> generateBalancedParentheses(int numPairs) {
+        char[] str = new char[2 * numPairs];
+        List<String> result = new LinkedList<>();
+        doGenerateBalancedParentheses(numPairs, numPairs, str, 0, result);
+        return result;
     }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  };
 
-  public static void main(String[] args) {
-    GenericTestHandler.executeTestsByAnnotation(
-        new Object() {}.getClass().getEnclosingClass(), args);
-  }
+    private static void doGenerateBalancedParentheses(int numLeftParens, int numRightParens,
+                                                      char[] str, int i, List<String> result) {
+        if (numLeftParens == 0 && numRightParens == 0) {
+            result.add(String.valueOf(str));
+            return;
+        }
+        // if there are left parens remaining, insert a left paren and recurse
+        if (numLeftParens > 0) {
+            str[i] = '(';
+            doGenerateBalancedParentheses(numLeftParens - 1, numRightParens, str, i + 1, result);
+        }
+        // if there are more right parens remaining, insert a right paren and recurse
+        if (numRightParens > numLeftParens) {
+            str[i] = ')';
+            doGenerateBalancedParentheses(numLeftParens, numRightParens - 1, str, i + 1, result);
+        }
+    }
+
+    @EpiTestComparator
+    public static BiPredicate<List<String>, List<String>> comp = (expected, result) -> {
+        if (result == null) {
+            return false;
+        }
+        Collections.sort(expected);
+        Collections.sort(result);
+        return expected.equals(result);
+    };
+
+    public static void main(String[] args) {
+        GenericTestHandler.executeTestsByAnnotation(
+                new Object() {}.getClass().getEnclosingClass(), args);
+    }
 }
